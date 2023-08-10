f9baa8601dd0998bae43f5c7909ce935f9d60e9b
kongds <j614023177@icloud.com>
2023-08-10 12:57:19 +0800

    update

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import sys
 import time
 import Quartz
+import threading
 import subprocess
 from pynput import keyboard
 from pynput.keyboard._darwin import KeyCode
 from pynput.keyboard import Key, Controller
 
 keycon = Controller()
+#DEBUG = True
+DEBUG = False
 
 # not map t
 app_key_map = {
@@ -76,8 +79,9 @@ def get_front_app():
 
 
 menu_hide = False
-def set_menu_visible(hide=False):
+def set_menu_visible(hide=False, force=False):
     global menu_hide
+    if force: menu_hide = not hide
     if hide:
         if not menu_hide:
             os.system("osascript -e 'tell application \"System Events\"' -e 'set autohide menu bar of dock preferences to true' -e 'end tell'")
@@ -92,6 +96,7 @@ hide_apps = {'微信': 'g', '音乐': 'm', '网易云音乐': 'n'}
 
 def spaceit(key_char):
     global spacedown
+    begin = time.time()
     if key_char in app_key_map:
         if key_char == 'e' and hide_menu_for_emacs:
             set_menu_visible(hide=True)
@@ -100,8 +105,9 @@ def spaceit(key_char):
         if  front_app in hide_apps and hide_apps[front_app] != key_char:
             os.system(f"osascript -e 'tell application \"Finder\"' -e 'set visible of process \"{front_app}\" to false' -e 'end tell'")
 
-        if key_char != 'e' and not os.path.exists('/Users/royokong/.eaf-showing'):
-            set_menu_visible(hide=False)
+        if key_char not in 'ei' and not os.path.exists('/Users/royokong/.eaf-showing'):
+            set_menu_visible(hide=False, force=True)
+        print('open app', time.time() - begin)
     elif key_char in cond_key_map:
         key_cmd = cond_key_map[key_char]()
         if type(key_cmd) is str:
@@ -118,7 +124,8 @@ def spaceit(key_char):
 def on_press(key):
     global spacedown
     global disable_p_once, spaceotherkey, cmd_down
-    print('p', key)
+    if DEBUG:
+        print('p', key)
 
     if key in [Key.cmd, Key.cmd_r, Key.cmd_l]:
         cmd_down = True
@@ -130,7 +137,8 @@ def on_press(key):
         else:
             disable_p_once = False
     elif spacedown and hasattr(key, 'char'):
-        spaceit(key.char)
+        # on_press, on_release should not block
+        threading.Thread(target=spaceit, args=(key.char,)).start()
         spaceotherkey = True
 
 kill_self = False
@@ -139,11 +147,14 @@ def on_release(key):
     global spacedown, restar_listener, kill_self
     global disable_r_once, disable_p_once, cmd_down
 
-    print('r', key)
+    if DEBUG:
+        print('r', key)
     if hasattr(key, 'vk') and key.vk == 0 and key.char != 'a':
         try:
-            print('restart listener')
-            kill_self = True
+            import datetime
+            current_time = datetime.datetime.now()
+            print(current_time, 'restart listener')
+            # kill_self = True
             # restar_listener = True
         except AttributeError:
             pass
@@ -164,10 +175,11 @@ def on_release(key):
             disable_r_once = False
 
         for cmd in wait_space_up_cmds:
-            os.system(cmd)
+            threading.Thread(target=os.system, args=(cmd,)).start()
         wait_space_up_cmds.clear()
     elif spacedown == True:
-        print('{0} released'.format(key), disable_space, key, type(key))
+        if DEBUG:
+            print('{0} released'.format(key), disable_space, key, type(key))
 
 def darwin_intercept(event_type, event):
     length, chars = Quartz.CGEventKeyboardGetUnicodeString(
@@ -221,7 +233,8 @@ while not kill_self:
         listener.start()
 
     time.sleep(0.2)
-    if len(subprocess.check_output('m1ddc display list'.split())) == 0:
+    display_list = subprocess.check_output('m1ddc display list'.split()).decode('utf-8')
+    if len(display_list) == 0: #or 'DELL U2412M' in display_list:
         hide_menu_for_emacs = False
     else:
         hide_menu_for_emacs = True
@@ -231,8 +244,8 @@ while not kill_self:
     if front_app == 'Emacs' and hide_menu_for_emacs:
         set_menu_visible(hide=True)
     elif not os.path.exists('/Users/royokong/.eaf-showing'):
+        #set_menu_visible(hide=False)
         pass
-        ##set_menu_visible(hide=False)
 
     if front_app in disable_apps:
         disable_space = True
